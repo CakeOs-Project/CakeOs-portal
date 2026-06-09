167e1067cd8c2bac9ba755babb9e2be2ff3b8d42
User <user@example.com>
2026-06-08 22:05:57 -0500

    se agrego calendario

--- CakeOs-portal/src/app/app.routes.ts
+++ CakeOs-portal/src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './feature/security/login/pages/login-pages.component';
+import { InvoicePagesComponent } from './feature/business/invoice/invoice-pages.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
     component: Login,
-  }
+  },
+  {
+    path: 'facturas',
+    component: InvoicePagesComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'facturas',
+    pathMatch: 'full',
+  },
 ];
-
